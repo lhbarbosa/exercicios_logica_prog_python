3643a51008749a7689abefda2131b4d73741f3d8
lhbarbosa <lehbarbosa87@gmail.com>
2024-05-31 20:15:53 -0300

    .

--- exercicios/desafio77.py
+++ exercicios/desafio77.py
@@ -2,16 +2,13 @@ lista_palavra = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'grati
                'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
 
 for palavra in lista_palavra:
-<<<<<<< HEAD
     print(f'\nNa palavra \033[33m{palavra.upper()}\033[m temos ', end='')
     for vogais  in palavra:
         if vogais.lower() in 'aeiou':
             print(f'\033[32m{vogais}\033[m', end=' ')
 print()
-=======
-   print(f'\nNa palavra \033[33m{palavra.upper()}\033[m temos ', end='')
-   for vogais in palavra:
+print(f'\nNa palavra \033[33m{palavra.upper()}\033[m temos ', end='')
+for vogais in palavra:
       if vogais.lower() in 'aeiou':
          print(f'\033[32m{vogais}\033[m', end=' ')
 print()
->>>>>>> f40d399772f65d8f4b4440deaa08f0712ab0e8d5
